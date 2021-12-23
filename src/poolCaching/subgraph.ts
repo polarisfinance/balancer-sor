e985947ed50a1885adf37d5951222d31ef1539a5
Daniel <danielBeets@protonmail.com>
2021-12-23 17:36:51 +0100

    Filter blacklisted pools on the client side, query is killing the subgraph performance

--- src/poolCaching/subgraph.ts
+++ src/poolCaching/subgraph.ts
@@ -1,6 +1,15 @@
 import fetch from 'isomorphic-fetch';
 import { SubgraphPoolBase } from '../types';
 
+const blacklistedPools = [
+    '0xae1c69eae0f1342425ea3fdb51e9f11223c7ad5b00010000000000000000000b',
+    '0x5018fa8aa910fa2eea07529d80e7a44b2e2d29cf000100000000000000000022',
+    '0xe2fd25b84aa76486e0cbc2c2ca383c3587abb942000100000000000000000028',
+    '0x51c5875ee17f1af4ddca0ce0df8dcad0b115b191000100000000000000000012',
+    '0xf61cb5126247dbadd1197651152a46f9b32678c40001000000000000000000d7',
+    '0xadba8867b6ff64ee650caf87dd3e943ed732901800010000000000000000001c',
+];
+
 // Returns all public pools
 export async function fetchSubgraphPools(
     subgraphUrl: string
@@ -10,7 +19,6 @@ export async function fetchSubgraphPools(
       {
         pools: pools(
           first: 1000,
-          where: { swapEnabled: true, id_not_in: ["0xae1c69eae0f1342425ea3fdb51e9f11223c7ad5b00010000000000000000000b", "0x5018fa8aa910fa2eea07529d80e7a44b2e2d29cf000100000000000000000022", "0xe2fd25b84aa76486e0cbc2c2ca383c3587abb942000100000000000000000028", "0x51c5875ee17f1af4ddca0ce0df8dcad0b115b191000100000000000000000012", "0xf61cb5126247dbadd1197651152a46f9b32678c40001000000000000000000d7", "0xadba8867b6ff64ee650caf87dd3e943ed732901800010000000000000000001c"] },
           orderBy: totalLiquidity,
           orderDirection: desc
         ) {
@@ -51,5 +59,7 @@ export async function fetchSubgraphPools(
 
     const { data } = await response.json();
 
-    return data.pools ?? [];
+    return (data.pools ?? []).filter(
+        (pool) => !blacklistedPools.includes(pool.id) && pool.swapEnabled
+    );
 }
