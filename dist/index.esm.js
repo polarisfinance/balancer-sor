8d35c73fd560ca9f6f3647cfbc28f928d3816b35
Daniel <danielBeets@protonmail.com>
2021-12-26 10:17:10 +0100

    rebuild the package

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -37007,14 +37007,6 @@ function getOnChainBalances(
     });
 }
 
-const blacklistedPools = [
-    '0xae1c69eae0f1342425ea3fdb51e9f11223c7ad5b00010000000000000000000b',
-    '0x5018fa8aa910fa2eea07529d80e7a44b2e2d29cf000100000000000000000022',
-    '0xe2fd25b84aa76486e0cbc2c2ca383c3587abb942000100000000000000000028',
-    '0x51c5875ee17f1af4ddca0ce0df8dcad0b115b191000100000000000000000012',
-    '0xf61cb5126247dbadd1197651152a46f9b32678c40001000000000000000000d7',
-    '0xadba8867b6ff64ee650caf87dd3e943ed732901800010000000000000000001c',
-];
 // Returns all public pools
 function fetchSubgraphPools(subgraphUrl) {
     var _a;
@@ -37022,11 +37014,7 @@ function fetchSubgraphPools(subgraphUrl) {
         // can filter for publicSwap too??
         const query = `
       {
-        pools: pools(
-          first: 1000,
-          orderBy: totalLiquidity,
-          orderDirection: desc
-        ) {
+        pools {
           id
           address
           poolType
@@ -37061,9 +37049,7 @@ function fetchSubgraphPools(subgraphUrl) {
             }),
         });
         const { data } = yield response.json();
-        return ((_a = data.pools) !== null && _a !== void 0 ? _a : []).filter(
-            (pool) => !blacklistedPools.includes(pool.id) && pool.swapEnabled
-        );
+        return (_a = data.pools) !== null && _a !== void 0 ? _a : [];
     });
 }
 
